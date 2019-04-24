d62dd2c90f04c1a08f0634cc30db875204fdf732
Michael Ethridge <maethridge@gmail.com>
2019-04-23 20:13:57 -0400

    I1: generates a random number

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
+require 'sinatra/reloader'
+
+def random_number
+    rand(101)
+end
+
+secret_number = random_number
 
 get '/' do
-    "Hello, World!"
+    "The secret number is: #{secret_number}"
 end
